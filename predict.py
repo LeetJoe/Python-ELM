1f7e3ee24c8aa2476776dd8721e01538bdd0c7f6
LeetJoe <tjusongchao@outlook.com>
2023-11-29 19:56:35 +0800

    sample predict

---
+++ predict.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import time
+import numpy as np
+import data_utils as dus
+from elm import GenELMClassifier
+from random_layer import MLPRandomLayer
+
+
+def pred_save(model, col_clip, in_file, out_file):
+    data = np.loadtxt(in_file, dtype=np.float64, delimiter=',', unpack=False)
+    X = data[:, 1:]
+    X = np.delete(X, col_clip, 1)
+
+    # normalization
+    X = dus.normalize(X)
+    y = model.predict(X)
+
+    with open(out_file, 'w') as fo:
+        fo.write("id,label\n")
+        for r in range(len(y)):
+            fo.write("{},{}\n".format(r + 1, np.round(y[r], 2)))
+
+        fo.close()
+
+
+ps_thresh = 0.01  # pearson rate & spearman rate threshold
+ol_percent = 0.1  # outlier percentage
+act_func = 'sigmoid'
+
+data = np.loadtxt('data/dataTrain_test.csv', dtype=np.float64, delimiter=',', unpack=False)
+
+X = data[:, 1:-1]
+y = data[:, -1]
+
+# normalization
+X = dus.normalize(X)
+
+idx_clip = dus.clip_list(X, ps_thresh)
+X_clip = np.delete(X, idx_clip, 1)
+idx_outliers = dus.idx_outlier(X_clip[:50000, :], ol_percent)
+X_dense = np.delete(X_clip[:50000, :], idx_outliers, 0)
+y_dense = np.delete(y[:50000], idx_outliers, 0)
+
+hn = 1000
+sig_rl = MLPRandomLayer(n_hidden=hn, activation_func=act_func)
+clf = GenELMClassifier(hidden_layer=sig_rl)
+
+s_time = time.time()
+clf.fit(X_dense, y_dense)
+score = np.round(clf.score(X_dense, y_dense)*100, 2)
+c_time = np.round(time.time() - s_time, 2)
+print("func: {}, hn: {}, score: {}, time: {}".format('sigmoid', hn, score, c_time))
+
+pred_save(clf, idx_clip, 'data/dataA_test.csv', 'data/predictA.csv')
+pred_save(clf, idx_clip, 'data/dataB_test.csv', 'data/predictB.csv')
--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import time
+import gc
 import numpy as np
 import data_utils as dus
 from elm import GenELMClassifier
 from random_layer import MLPRandomLayer
 
+ps_thresh = 0.01  # pearson rate & spearman rate threshold
+ol_percent = 0.1  # outlier percentage
+act_func = 'sigmoid'
 
 data = np.loadtxt('data/dataTrain_test.csv',dtype=np.float64,delimiter=',',unpack=False)
 
 X = data[:, 1:-1]
 y = data[:, -1]
-nrow, ncol = X.shape
 
 # normalization
 X = dus.normalize(X)
@@ -19,27 +22,27 @@ X = dus.normalize(X)
 data_group = {'original': (X, y)}
 
 # 
-idx_clip = dus.clip_list(X, 0.01)
+idx_clip = dus.clip_list(X, ps_thresh)
 X_clip = np.delete(X, idx_clip, 1)
 data_group['clip'] = (X_clip, y)
 
+# todo AUC filter
 data_group['filter'] = (X[:50000, :], y[:50000])
 data_group['clip&filter'] = (X_clip[:50000, :], y[:50000])
 
-idx_outliers = dus.idx_outlier(X_clip[:50000, :], 0.1)
+idx_outliers = dus.idx_outlier(X_clip[:50000, :], ol_percent)
 X_dense = np.delete(X_clip[:50000, :], idx_outliers, 0)
 y_dense = np.delete(y[:50000], idx_outliers, 0)
 data_group['dense'] = (X_dense, y_dense)
 
 result = [['func', 'h-num', 'data', 'score', 'time']]
 
-hn_list = [500, 1000, 2000, 4000, 8000, 12000]
-# hn_list = [1000]
+hn_list = [500, 1000, 2000, 4000, 8000, 10000]
 
 for hn in hn_list:
-    sig_rl = MLPRandomLayer(n_hidden=hn, activation_func='sigmoid')
-    clf = GenELMClassifier(hidden_layer=sig_rl)
+    sig_rl = MLPRandomLayer(n_hidden=hn, activation_func=act_func)
     for dk in data_group:
+        clf = GenELMClassifier(hidden_layer=sig_rl)
         cur_X, cur_y = data_group[dk]
         s_time = time.time()
         clf.fit(cur_X, cur_y)
@@ -47,6 +50,8 @@ for hn in hn_list:
         c_time = np.round(time.time() - s_time, 2)
         result.append(['sigmoid', hn, dk, score, c_time])
         print("func: {}, hn: {}, data: {}, score: {}, time: {}".format('sigmoid', hn, dk, score, c_time))
+        del clf
+        gc.collect()
 
 with open('data/result.csv', 'w') as fo:
     for r in result:
