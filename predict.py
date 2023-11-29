8e7c892766cbbbdde9089248800ea8cd689a922d
LeetJoe <tjusongchao@outlook.com>
2023-11-29 21:07:10 +0800

    add checkpoint save&load

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import time
+import os
+import pickle
 import numpy as np
 import data_utils as dus
 from elm import GenELMClassifier
@@ -27,6 +29,9 @@ def pred_save(model, col_clip, in_file, out_file):
 ps_thresh = 0.01  # pearson rate & spearman rate threshold
 ol_percent = 0.1  # outlier percentage
 act_func = 'sigmoid'
+hn = 8000
+save_model = False
+load_model = False
 
 data = np.loadtxt('data/dataTrain_test.csv', dtype=np.float64, delimiter=',', unpack=False)
 
@@ -42,15 +47,29 @@ idx_outliers = dus.idx_outlier(X_clip[:50000, :], ol_percent)
 X_dense = np.delete(X_clip[:50000, :], idx_outliers, 0)
 y_dense = np.delete(y[:50000], idx_outliers, 0)
 
-hn = 8000
-sig_rl = MLPRandomLayer(n_hidden=hn, activation_func=act_func)
-clf = GenELMClassifier(hidden_layer=sig_rl)
+model_file = 'data/model_{}.sav'.format(hn)
 
 s_time = time.time()
-clf.fit(X_dense, y_dense)
+if load_model:
+    if not os.path.exists(model_file):
+        print('Error! Model file {} not found.'.format(model_file))
+        exit(-1)
+    with open(model_file, 'rb') as fi:
+        clf = pickle.load(fi)
+        print('Model loaded from {}.'.format(model_file))
+else:
+    sig_rl = MLPRandomLayer(n_hidden=hn, activation_func=act_func)
+    clf = GenELMClassifier(hidden_layer=sig_rl)
+    clf.fit(X_dense, y_dense)
+
 score = np.round(clf.score(X_dense, y_dense)*100, 2)
 c_time = np.round(time.time() - s_time, 2)
 print("func: {}, hn: {}, score: {}, time: {}".format('sigmoid', hn, score, c_time))
+
+if (not load_model) and save_model:
+    with open(model_file, 'wb') as fo:
+        pickle.dump(clf, fo)
+    print('Saved model into {}...'.format(model_file))
 
 pred_save(clf, idx_clip, 'data/dataA_test.csv', 'data/predictA_{}.csv'.format(hn))
 pred_save(clf, idx_clip, 'data/dataB_test.csv', 'data/predictB_{}.csv'.format(hn))
